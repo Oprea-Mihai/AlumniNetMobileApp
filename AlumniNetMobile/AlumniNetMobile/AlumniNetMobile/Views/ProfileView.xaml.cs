97e9a6e41a2574c39184157d2546f43b13233fe6
Mihai Oprea <moprea@talentingsoftware.com>
2023-03-25 13:54:21 +0200

    Created MVVM pattern, working on profile page

---
+++ AlumniNetMobile/AlumniNetMobile/AlumniNetMobile/Views/ProfileView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AlumniNetMobileApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AlumniNetMobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfileView : ContentPage
+    {
+        public ProfileView()
+        {
+            InitializeComponent();
+            BindingContext = new ProfileViewModel();
+
+        }
+    }
+}
--- AlumniNetMobile/AlumniNetMobile/AlumniNetMobile/Views/SignUpView.xaml.cs
+++ AlumniNetMobile/AlumniNetMobile/AlumniNetMobile/Views/SignUpView.xaml.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace AlumniNetMobileApp
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AlumniNetMobileApp.Views
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignUpView : ContentPage
     {
-        public MainPage()
+        public SignUpView()
         {
             InitializeComponent();
         }
